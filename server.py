976c18c063421387436e284af07a1472333dc6e9
VinhNN747 <vinhngoc172005@gmail.com>
2026-03-18 16:12:23 +0700

    improved model by using cnn instead of fully connected nn.

--- server.py
+++ server.py
@@ -2,7 +2,13 @@ import numpy as np
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from matplotlib import pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from torch.utils.data import Dataset
+from torchvision import datasets, transforms
 
 app = FastAPI()
 
@@ -13,44 +19,62 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# load model
-data = np.load("model.npz", allow_pickle=True)
-W = data["weights"]
-b = data["biases"]
-
 
 class PixelInput(BaseModel):
     pixels: list
 
 
-def ReLU(Z):
-    return np.maximum(0, Z)
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 32, 3, padding=1),  # 28x28 -> 28x28
+            nn.ReLU(),
+            nn.MaxPool2d(2),  # -> 14x14
 
-def softmax(z):
-    e = np.exp(z - np.max(z))
-    return e / np.sum(e)
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)  # -> 7x7
+        )
+
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(64 * 7 * 7, 128),
+            nn.ReLU(),
+            nn.Linear(128, 10)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.fc(x)
+        return x
 
 
-def predict(x):
-    a = x.reshape(784, 1)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    for Wi, bi in zip(W[:-1], b[:-1]):
-        a = ReLU(Wi @ a + bi)
-
-    z = W[-1] @ a + b[-1]
-    a = softmax(z)
-
-    return int(np.argmax(a))
+model = CNN()
+model.load_state_dict(torch.load("mnist_cnn.pth", map_location=device))
+model.to(device)
+model.eval()
 
 
 @app.post("/predict")
 def predict_digit(data: PixelInput):
-    x = data.pixels
-    current_image = np.array(x).T / 255.
-    current_image = current_image.reshape((28, 28))
-    # plt.gray()
-    # plt.imshow(current_image, interpolation='nearest')
-    # plt.show()
-    digit = predict(current_image)
+    # 1. convert input
+    x = np.array(data.pixels).astype('float32') / 255.0
 
-    return {"digit": digit}
+    # 2. reshape đúng format CNN
+    x = torch.tensor(x).view(1, 1, 28, 28).to(device)
+
+    # 3. predict
+    with torch.no_grad():
+        output = model(x)
+
+        probs = torch.softmax(output, dim=1)
+        conf, pred = torch.max(probs, dim=1)
+
+    # 4. trả kết quả
+    return {
+        "digit": pred.item(),
+        "confidence": float(conf.item())
+    }
